18455b554857e2844aabdaac2aa8596d818efe7f
JoJosefe <147321018+Seifex01@users.noreply.github.com>
2023-10-18 20:30:31 +0200

    Tuve que quitar saltos de linea para que saliera el codigo entero dentro de la captura de pantalla

--- app/src/main/java/com/example/articulocompose/MainActivity.kt
+++ app/src/main/java/com/example/articulocompose/MainActivity.kt
@@ -21,7 +21,6 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,8 +41,6 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-
-
 @Composable
 fun ArticuloCompose(
     titulo: String,
@@ -72,11 +69,7 @@ fun ArticuloCompose(
         )
     }
 
-
 }
-
-
-
 @Preview(showBackground = true)
 @Composable
 fun ArticuloComposePreview() {
